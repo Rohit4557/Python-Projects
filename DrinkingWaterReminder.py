959103d2b3621a4b32ee6240eb43ae7a70f9ca3e
Rohit45 <rohitnautiyal59@gmail.com>
2025-04-18 22:47:37 +0530

    updated

--- DrinkingWaterReminder.py
+++ DrinkingWaterReminder.py
@@ -1,18 +1,20 @@
 #Drink Water Reminder
 
-from win11toast import toast
-from gtts import gTTS
-import os
-import time
+#bewlow are the all packanges and modules required for the project
+from playsound import playsound #for the soundplay and a good helper in deleting the file later without error
+from win11toast import toast #for the notification
+from gtts import gTTS #for the google text to speech conversion
+import os #for deletion
+import time #for time delays btw reminders
 
-reminders = 1
-message = "Rohit Bhaiya paani peelo"
+reminders = 2
+message = "Please Developer DRink THe WAter"
 
 tts = gTTS(text=message, lang="en")
 tts.save("remind.mp3")
 
 for i in range(reminders):
     toast(message)
-    os.system("start remind.mp3")
-    time.sleep(3)  # 10-minute gap
+    playsound("remind.mp3")
+    time.sleep(3)  # time gap in seconds
 os.remove("remind.mp3")
--- README.md
+++
@@ -1,1 +0,0 @@
-This repo contains all the small projects i have made in my initial phase
